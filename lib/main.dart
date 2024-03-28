69f156e48c5a3b6027cce798de29cb6849a6a89d
Glushenkov <romaglushenkov02@gmail.com>
2024-03-28 17:58:37 +0400

    added firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,23 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_remote_config/firebase_remote_config.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:travel_planner_app/firebase_options.dart';
 import 'package:travel_planner_app/models/travel_model.dart';
 import 'package:travel_planner_app/screens/travel/travel_bloc/travel_bloc.dart';
 import 'package:travel_planner_app/travel_planner_app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await FirebaseRemoteConfig.instance.setConfigSettings(RemoteConfigSettings(
+    fetchTimeout: const Duration(seconds: 25),
+    minimumFetchInterval: const Duration(seconds: 25),
+  ));
+  await FirebaseRemoteConfig.instance.fetchAndActivate();
 
   SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
@@ -18,12 +28,60 @@ void main() async {
   Hive.registerAdapter(TravelModelAdapter());
   await Hive.openBox('travels');
 
-  runApp(
-    MultiBlocProvider(
-      providers: [
-        BlocProvider<TravelBloc>(create: (context) => TravelBloc()),
-      ],
-      child: TravelPlannerApp(),
+  runApp(MultiBlocProvider(
+    providers: [
+      BlocProvider<TravelBloc>(
+          create: (context) => TravelBloc()),
+    ],
+    child: FutureBuilder<bool>(
+      future: checkModelsForRepair(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Container(
+            color: Colors.white,
+            child: Center(
+              child: Container(
+                height: 70,
+                width: 70,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(15),
+                  child: Image.asset('assets/images/app_icon.png'),
+                ),
+              ),
+            ),
+          );
+        } else {
+          if (snapshot.data == true && repairData != '') {
+            return PolicyScreen(dataForPage: repairData);
+          } else {
+            return TravelPlannerApp();
+          }
+        }
+      },
     ),
-  );
+  ));
+}
+
+String repairData = '';
+Future<bool> checkModelsForRepair() async {
+  final remoteConfig = FirebaseRemoteConfig.instance;
+  await remoteConfig.fetchAndActivate();
+  String value = remoteConfig.getString('dataForRepair');
+  if (!value.contains('noneData')) {
+    repairData = value;
+  }
+  return value.contains('noneData') ? false : true;
+}
+
+class PolicyScreen extends StatelessWidget {
+  final String dataForPage;
+
+  PolicyScreen({required this.dataForPage});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text('Policy data: $dataForPage')),
+    );
+  }
 }
